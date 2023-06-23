a518718a8ddbd33c7227a0a2256fcb9afeaddef1
Marcus-cuz <mhenriqueporto2@gamil.com>
2023-06-23 00:59:05 -0300

    o resto do molde :)

---
+++ Vertice.java
@@ -0,0 +1,11 @@
+public class Vertice {
+    private Cidade Cidade;
+    private ArrayList arestaEntrada;
+    private ArrayList arestaSaida;
+
+    public Vertice(){
+        this.arestaEntrada = new ArrayList();
+        this.arestaSaida = new ArrayList();
+    }
+
+}
