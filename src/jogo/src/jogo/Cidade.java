e45d66dca863a58fa45edad1870cdf0d91ae5c0e
Marcus-cuz <mhenriqueporto2@gamil.com>
2023-06-27 16:21:40 -0300

    criando cidades e caminhos

--- src/jogo/src/jogo/Cidade.java
+++ src/jogo/src/jogo/Cidade.java
@@ -3,24 +3,35 @@ package src.jogo;
 public class Cidade {
     private String nome;
     private LimiarPedra pesoPedra;
-    private Boolean quest;
+    private MissaoRecompensa quest;
+
+    public  Cidade(String nome, LimiarPedra pesoPedra, MissaoRecompensa quest) {
+        this.nome = nome;
+        this.pesoPedra = pesoPedra;
+        this.quest = quest;
+    }
 
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public LimiarPedra getPesoPedra() {
         return pesoPedra;
     }
+
     public void setPesoPedra(LimiarPedra pesoPedra) {
         this.pesoPedra = pesoPedra;
     }
-    public Boolean getQuest() {
+
+    public MissaoRecompensa getQuest() {
         return quest;
     }
-    public void setQuest(Boolean quest) {
+
+    public void setQuest(MissaoRecompensa quest) {
         this.quest = quest;
     }
 
